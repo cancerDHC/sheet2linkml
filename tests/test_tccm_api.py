1bd7f03be21156fa522f875a1657354a28a6643c
Sujay Patil <sujaysanjeev.patil@gmail.com>
2021-11-19 12:04:37 -0800

    add test cases for get_enum_values_for_field()

--- tests/test_tccm_api.py
+++ tests/test_tccm_api.py
@@ -1,8 +1,24 @@
 # test_tccm_api.py - Test whether the TCCM API works
+import pytest
 
 from sheet2linkml.terminologies.tccm.api import TCCMService
 
 
-def test_tccm_service():
-    tccm = TCCMService('https://terminology.ccdh.io')
-    assert tccm.base_url == 'https://terminology.ccdh.io'
+class TestTCCMService:
+    def test_tccm_service(self):
+        tccm = TCCMService('https://terminology.ccdh.io')
+        assert tccm.base_url == 'https://terminology.ccdh.io'
+
+    @pytest.mark.skip("CCDH Terminology Service is down")
+    def test_get_enum_values_for_field_failure(self):
+        tccm = TCCMService('https://terminology.ccdh.io')
+
+        assert tccm.get_enum_values_for_field("CRDC-H:Foo.bar") == {}
+
+    def test_get_enum_values_for_field_enum_not_exist(self):
+        tccm = TCCMService('https://terminology.ccdh.io')
+
+        with pytest.raises(RuntimeError) as excinfo:
+            tccm.get_enum_values_for_field("CRDC-H:CancerStageObservation.id")
+
+        assert "Error accessing TCCM Terminology Service" in str(excinfo.value)

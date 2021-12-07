3af8e51afbbea2cce1eda4066f3538fbfed2a159
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-12-07 02:18:20 -0500

    Added tests for generating a schema from a Google Sheet.

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -3,6 +3,8 @@ test_command_line.py: Test the command line interfaces of this program.
 """
 
 import subprocess
+import logging
+import os
 
 def execute_sheet2linkml(args) -> (int, str, str):
     """
@@ -33,3 +35,51 @@ class TestCommandLine:
         assert '--help' in stdout
         assert 'Show this message and exit.' in stdout
         assert 'ERROR' not in stderr
+
+    def test_execution(self, tmp_path):
+        """
+        Test whether sheet2linkml can be used to process a particular Google Sheet into LinkML, which is located at
+        https://docs.google.com/spreadsheets/d/1HpsF12vl_CeIzV2TEtANeysnh7cy6YOUDdv6fMXT00I/edit?usp=sharing
+        """
+
+        output_path = os.path.join(tmp_path, 'schema.yaml')
+
+        print(f"Writing execution test output to '{output_path}'")
+
+        (errno, stdout, stderr) = execute_sheet2linkml([
+            '--google-sheet-id', '1HpsF12vl_CeIzV2TEtANeysnh7cy6YOUDdv6fMXT00I',
+            '-o', output_path
+        ])
+
+        assert 'INFO: Google Sheet loaded: GSheetModel with an underlying Google Sheet titled "Test schema for sheet2linkml" containing 8 worksheets' in stderr
+        assert 'INFO: Generating LinkML for GSheetModel with an underlying Google Sheet titled "Test schema for sheet2linkml" containing 8 worksheets' in stderr
+        assert 'INFO: Generating LinkML for Enum named Subject.breed from worksheet "O_CCDH Enums" containing 8 values' in stderr
+
+        assert stdout == ''
+        assert errno == 0
+
+        assert os.path.exists(output_path)
+
+        # Ensure that we get the same output from sheet2linkml as we expected.
+        def make_schema_comparable(lines):
+            """
+            Some parts of the schema are not comparable, so we remove them before comparing them.
+            """
+
+            output = ""
+            for line in lines:
+                if line.startswith('generation_date:'):
+                    continue
+                output += line + '\n'
+
+            return output
+
+        dirname = os.path.dirname(__file__)
+        with open(os.path.join(dirname, 'testing-schema-on-gsheet/1HpsF12vl_CeIzV2TEtANeysnh7cy6YOUDdv6fMXT00I.yaml'), 'r') as f:
+            expected_schema = make_schema_comparable(f.readlines())
+
+        with open(output_path, 'r') as f:
+            obtained_schema = make_schema_comparable(f.readlines())
+
+        assert obtained_schema != ''
+        assert obtained_schema == expected_schema
